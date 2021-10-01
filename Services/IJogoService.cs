4af6f488cccdf999cded092816d4c69628dcf455
andreispanier <83679288+andreispanier@users.noreply.github.com>
2021-09-30 22:31:28 -0300

    Add files via upload
    Projeto feito de modo passo a passo de como criar uma API com DotNet.

---
+++ Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.ImputModel;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
